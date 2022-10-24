3bb01d4025102d4048dd03864dd60b94887f16f2
hyemin0303 <hyeminpak@naver.com>
2022-10-24 23:39:07 +0900

    Create project Parcel test

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+//import autoprefixer from 'autoprefixer' 와 동일
+//const autoprefixer = require('autoprefixer')
+
+//  export {
+//   plugins: [
+//     autoprefixer
+//   ]
+//  }
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
